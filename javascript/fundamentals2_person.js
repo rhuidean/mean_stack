4cff10b20bee8bf2941c15b31305738a471eb0c0
Jason <rhuidean06@gmail.com>
2017-05-10 12:49:17 -0500

    add javascript person class

---
+++ javascript/fundamentals2_person.js
@@ -0,0 +1,33 @@
+function Person(name) {
+	this.name = name;
+	this.distance_traveled = 0;
+
+	this.say_name = function() {
+		console.log(this.name);
+	}
+
+	this.say_something = function(parameter) {
+		console.log(this.name,"says",parameter);
+	}
+	
+	this.walk = function() {
+		console.log(this.name, "is walking");
+		this.distance_traveled += 3;
+	}
+
+	this.run = function() {
+		console.log(this.name, "is running");
+		this.distance_traveled += 10;
+	}
+
+	this.crawl = function() {
+		console.log(this.name, "is crawling");
+		this.distance_traveled += 1;
+	}
+
+}
+
+person1 = new Person("Jason")
+person1.say_name()
+person1.say_something("Hello MD!")
+
